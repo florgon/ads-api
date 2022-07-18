4af0715ddab51f724f83e97f116257cfe6ec8e40
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-07-18 21:29:55 +0400

    Remove ads.getViewBlock client_id param.

--- src/ads-api/app/routers/ads.py
+++ src/ads-api/app/routers/ads.py
@@ -12,28 +12,21 @@ router = APIRouter()
 
 
 @router.get("/ads.getViewBlock")
-async def method_ads_get_view_block(client_id: int = 0, renderer: str | None = None) -> HTMLResponse | JSONResponse:
+async def method_ads_get_view_block(renderer: str | None = None) -> HTMLResponse | JSONResponse:
     """Returns HTML/JS/CSS of the view block of the ad. Should be called by JS-library on the client website."""
 
     if renderer is None:
         renderer = "html"
-        
+
     if renderer == "html":
-        return ads_view_block_html_renderer(client_id)
+        return ads_view_block_html_renderer()
     elif renderer == "js":
-        return ads_view_block_js_renderer(client_id)
+        return ads_view_block_js_renderer()
     else:
         return api_error(ApiErrorCode.API_INVALID_REQUEST, "renderer must be 'html' or 'js'")
 
 
-def ads_view_block_js_renderer(client_id: int) -> JSONResponse:
-    if client_id <= 0:
-        return api_error(ApiErrorCode.API_INVALID_REQUEST, "Client_id invalid.", {
-            "view_block": {
-                "type": "text",
-                "data": "Florgon Ads improperly configured! Client_id invalid."
-            }
-        })
+def ads_view_block_js_renderer() -> JSONResponse:
     return api_success({
         "view_block": {
             "type": "text",
@@ -42,19 +35,7 @@ def ads_view_block_js_renderer(client_id: int) -> JSONResponse:
     })
 
 
-def ads_view_block_html_renderer(client_id: int) -> HTMLResponse:
-    if client_id <= 0:
-        return HTMLResponse("""
-            <html>
-                <head>
-                    <style></style>
-                </head>
-                <body>
-                    Florgon Ads improperly configured! Review your client_id.
-                </body>
-            </html>
-        """)
-        
+def ads_view_block_html_renderer() -> HTMLResponse:
     return HTMLResponse("""
         <html>
             <head>
