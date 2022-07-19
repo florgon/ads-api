e8664384924b64a12dd89f7b9f047b425766da37
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-07-19 08:23:12 +0400

    Fix html renderer css block.

--- src/ads-api/app/renderers/html.py
+++ src/ads-api/app/renderers/html.py
@@ -10,27 +10,32 @@ def ads_view_block_html_renderer(ad: Ad) -> HTMLResponse:
         ad_text = ad.text
         ad_link = f"https://ads.florgon.space/c?aid={ad.id}"
 
-    return HTMLResponse("""
+    css = """
+        html, body { 
+            margin: 0; 
+            padding: 0; 
+            width: 100%; 
+            height: 100%; 
+        }
+        a { 
+            display: block; 
+            width: 100%; 
+            height: 100%; 
+            text-decoration: none; 
+            color: black;
+        }
+    """
+    html = """
         <html>
             <head>
                 <style>
-                    html, body { 
-                        margin: 0; 
-                        padding: 0; 
-                        width: 100%; 
-                        height: 100%; 
-                    }
-                    a { 
-                        display: block; 
-                        width: 100%; 
-                        height: 100%; 
-                        text-decoration: none; 
-                        color: black;
-                    }
+                    {css}
                 </style>
             </head>
             <body>
                 <a href='{ad_link}'>{ad_text}</a>
             </body>
         </html>
-    """.format(ad_link=ad_link, ad_text=ad_text))
+    """
+    html = html.format(css=css, ad_link=ad_link, ad_text=ad_text)
+    return HTMLResponse(content=html)
