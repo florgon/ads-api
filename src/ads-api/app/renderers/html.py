a907f464c8917c510ddaa4fc8a76ea8ce57bc557
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-07-19 08:11:15 +0400

    Renderer use links, return aid.

--- src/ads-api/app/renderers/html.py
+++ src/ads-api/app/renderers/html.py
@@ -7,15 +7,28 @@ def ads_view_block_html_renderer(ad: Ad) -> HTMLResponse:
         ad_text = "Sorry, no ad was found for you! =("
     else:
         ad_text = ad.text
+
     return HTMLResponse("""
         <html>
             <head>
-                <style></style>
+                <style>
+                    html, body { 
+                        margin: 0; 
+                        padding: 0; 
+                        width: 100%; 
+                        height: 100%; 
+                    }
+                    a { 
+                        display: block; 
+                        width: 100%; 
+                        height: 100%; 
+                        text-decoration: none; 
+                        color: black;
+                    }
+                </style>
             </head>
             <body>
-                <span>
-                    {ad_text}
-                </span>
+                <a href='https://ads.florgon.space/c?aid={ad_id}'>{ad_text}</a>
             </body>
         </html>
-    """.format(ad_text=ad_text))
+    """.format(ad_id=ad.id, ad_text=ad_text))
--- src/ads-api/app/renderers/js.py
+++ src/ads-api/app/renderers/js.py
@@ -8,12 +8,20 @@ def ads_view_block_js_renderer(ad: Ad) -> JSONResponse:
         return api_success({
             "view_block": {
                 "type": "text",
-                "data": "Sorry, no ad was found for you! =("
+                "data": "Sorry, no ad was found for you! =(",
+                "link": "#"
+            },
+            "payload": {
+                "aid": 0
             }
         })
     return api_success({
         "view_block": {
             "type": "text",
-            "data": ad.text
+            "data": ad.text,
+            "link": f"https://ads.florgon.space/c?aid={ad.id}"
+        },
+        "payload": {
+            "aid": ad.id
         }
     })
